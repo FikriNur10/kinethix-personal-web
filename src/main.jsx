c24ae0c6c4397349739c83fdec089eb91f8a6bd6
Fikri Nur Diega <79088843+FikriNur10@users.noreply.github.com>
2024-10-24 21:12:09 +0700

    fix:deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import SliceOfArt from "./pages/projectPage/sliceofart.jsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/project/sliceofart",
-    element: <SliceOfArt />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
